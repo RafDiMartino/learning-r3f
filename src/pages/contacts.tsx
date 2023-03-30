d67ba1492cfbd1c8a7111832d6f24390caeda32c
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-30 13:52:00 +0100

    adding contact and projects pages

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Raf Di Martino | Contacts</title>
+        <meta name="description" content="Raf Di Martino | Web Development Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className="flex-grow text-center font-josefin">Contacts</div>
+    </> 
+  )
+}
