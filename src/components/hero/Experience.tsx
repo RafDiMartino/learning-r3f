9ceef572db1645e131b8ea2af8aac11c8c6953e2
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-03 19:11:55 +0100

    fixing ts errors

--- src/components/hero/Experience.tsx
+++ src/components/hero/Experience.tsx
@@ -1,24 +1,20 @@
-import { extend, useFrame, useThree, Object3DNode } from '@react-three/fiber'
-import { log } from 'console'
+import { extend, useFrame, useThree, ReactThreeFiber } from '@react-three/fiber'
 import React, { useRef } from 'react'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 extend({ OrbitControls })
 
-declare module '@react-three/fiber' {
-    interface ThreeElements {
-      customElement: Object3DNode<OrbitControls, typeof OrbitControls>
+declare global {
+    namespace JSX {
+      interface IntrinsicElements {
+        'orbitControls': ReactThreeFiber.Object3DNode<OrbitControls, typeof OrbitControls>;
+      }
     }
-  }
+}
 
 export const Experience = () => {
     
     const { camera, gl} = useThree()
-
-    // const three = new OrbitControls(camera, gl.domElement)
-
-    console.log(camera, gl);
-    
 
     const cubeRef: any = useRef()
     const groupRef: any = useRef()
@@ -32,14 +28,14 @@ export const Experience = () => {
         <>
             <orbitControls args={ [ camera, gl.domElement ] }/>
             <group ref={groupRef}>
-                <mesh position-x={-2}>
+                <mesh position-x={-2} scale={ 1 }>
                     {/* <sphereGeometry args={ [ 1.5, 32, 32 ] }/> //radius, widthSegments, heightSegment */}
                     <sphereGeometry />
                     <meshBasicMaterial color="orange" wireframe={false} />
                 </mesh>
 
                 <mesh ref={ cubeRef } rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-                    <boxGeometry scale={1.5} />
+                    <boxGeometry  />
                     <meshBasicMaterial color="purple" wireframe={false} />
                 </mesh>
             </group>
