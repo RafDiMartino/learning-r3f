16538ff8d6a1e83a8757a785c028ec42dad9451b
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-03 16:21:23 +0100

    group basics

--- src/components/hero/Experience.tsx
+++ src/components/hero/Experience.tsx
@@ -4,24 +4,27 @@ import React, { useRef } from 'react'
 export const Experience = () => {
 
     const cubeRef: any = useRef()
+    const groupRef: any = useRef()
 
     useFrame((state, delta) => {
         cubeRef.current.rotation.y += delta;
-        
+        // groupRef.current.rotation.y += delta;
     })
     
     return (
         <>
-            <mesh position-x={-2}>
-                {/* <sphereGeometry args={ [ 1.5, 32, 32 ] }/> //radius, widthSegments, heightSegment */}
-                <sphereGeometry />
-                <meshBasicMaterial color="orange" wireframe={false} />
-            </mesh>
+            <group ref={groupRef}>
+                <mesh position-x={-2}>
+                    {/* <sphereGeometry args={ [ 1.5, 32, 32 ] }/> //radius, widthSegments, heightSegment */}
+                    <sphereGeometry />
+                    <meshBasicMaterial color="orange" wireframe={false} />
+                </mesh>
 
-            <mesh ref={ cubeRef } rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-                <boxGeometry scale={1.5} />
-                <meshBasicMaterial color="purple" wireframe={false} />
-            </mesh>
+                <mesh ref={ cubeRef } rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+                    <boxGeometry scale={1.5} />
+                    <meshBasicMaterial color="purple" wireframe={false} />
+                </mesh>
+            </group>
 
             <mesh position-y={-1} rotation-x={- Math.PI * 0.5} scale={10}>
                 <planeGeometry />
