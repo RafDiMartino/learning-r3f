1c0f68041fa1b2f298d645f37902da119432b3f8
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-04 16:12:54 +0100

    exploring  drei helper TransformControls

--- src/components/hero/Experience.tsx
+++ src/components/hero/Experience.tsx
@@ -1,11 +1,15 @@
-import {OrbitControls} from '@react-three/drei'
+import { OrbitControls, TransformControls } from '@react-three/drei'
+import { useRef } from 'react'
 
-export default function Experience()
-{
+
+export default function Experience(){
+
+
+    const cube: any = useRef()
 
     return (
     <>
-        <OrbitControls />
+        <OrbitControls makeDefault/>
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
@@ -14,10 +18,11 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+        <TransformControls object={cube} mode='translate'/>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
