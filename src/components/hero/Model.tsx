13e744a9e2a7e797ea6ed6c83f164bd549c3368f
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-11 17:34:11 +0100

    GLTF loading with drei

--- src/components/hero/Model.tsx
+++ src/components/hero/Model.tsx
@@ -1,24 +1,29 @@
+import { useGLTF } from '@react-three/drei'
 import React from 'react'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
-import { useLoader } from '@react-three/fiber'
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+// import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+// import { useLoader } from '@react-three/fiber'
 
 export default function Model() {
 
     // const model = useLoader(GLTFLoader, '/hamburger.glb')
 
-    const model = useLoader(
-        GLTFLoader,
-        '/hamburger.glb',
-        (loader) => {
+    // const model = useLoader(
+    //     GLTFLoader,
+    //     '/hamburger.glb',
+    //     (loader) => {
 
-            const dracoLoader = new DRACOLoader()
-            dracoLoader.setDecoderPath('/draco/')
-            loader.setDRACOLoader(dracoLoader)
-        }
-    )
+    //         const dracoLoader = new DRACOLoader()
+    //         dracoLoader.setDecoderPath('/draco/')
+    //         loader.setDRACOLoader(dracoLoader)
+    //     }
+    // )
+
+    const model = useGLTF('/hamburger.glb')
 
     return (
         <primitive object={ model.scene } scale={ 0.35 } />
     )
 }
+
+useGLTF.preload('/hamburger.glb')
