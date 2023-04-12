073247182c2af2db3a84e9f5d8c8b4e3ca89e4e4
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-12 13:23:23 +0100

    gltf to component

--- src/components/hero/Experience.tsx
+++ src/components/hero/Experience.tsx
@@ -3,6 +3,7 @@ import { Perf } from 'r3f-perf'
 import Model from './Model'
 import { Suspense } from 'react'
 import Placeholder from './Placeholder'
+// import Hamburger from './Hamburger'
 
 export default function Experience() {
 
@@ -14,7 +15,7 @@ export default function Experience() {
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} shadow-normalBias={0.04} />
         <ambientLight intensity={0.5} />
 
         <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
@@ -27,6 +28,7 @@ export default function Experience() {
             }
         >
             <Model />
+            {/* <Hamburger scale={0.35} /> */}
         </Suspense>
     </>
 }
