4bbad6d0212231df571a5cf5c18d05f122d9b8b9
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-19 17:24:04 +0100

    apply forces

--- src/components/physics/PhysicsExperience.jsx
+++ src/components/physics/PhysicsExperience.jsx
@@ -1,8 +1,18 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { BallCollider, CuboidCollider, Debug, RigidBody, Physics } from '@react-three/rapier'
+import { useRef } from 'react'
+
 
 export default function PhysicsExperience() {
+
+    const cube = useRef()
+
+    const cubeJump = () => {
+        cube.current.applyImpulse({x: 0, y: 5, z: 0})
+        cube.current.applyTorqueImpulse({x: Math.random() -0.5, y: Math.random() -0.5, z: Math.random() -0.5})
+    }
+
     return <>
 
         <Perf position="top-left" />
@@ -16,32 +26,28 @@ export default function PhysicsExperience() {
             <Debug />
 
             <RigidBody colliders="ball">
-                <mesh castShadow position={[0, 4, 0]}>
+                <mesh castShadow position={[-1.5, 2, 0]}>
                     <sphereGeometry />
                     <meshStandardMaterial color="orange" />
                 </mesh>
             </RigidBody>
 
-            {/* <RigidBody >
-                <mesh castShadow position={[2, 2, 0]}>
-                    <boxGeometry args={[3, 2, 1]} />
+            <RigidBody ref={cube} position={[1.5, 2, 0]}>
+                <mesh castShadow onClick={cubeJump}>
+                    <boxGeometry />
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
-                <mesh castShadow position={[2, 2, 3]}>
-                    <boxGeometry args={[1, 1, 1]} />
-                    <meshStandardMaterial color="mediumpurple" />
-                </mesh>
-            </RigidBody> */}
+            </RigidBody>
 
-            <RigidBody colliders={false} position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
-                {/* <CuboidCollider args={[1.5,1.5,0.5]}/>
-                <CuboidCollider args={[0.25,1,0.25]} position={[0,0,1]} rotation={[-Math.PI * 0.35, 0, 0]}/> */}
+            {/* <RigidBody colliders={false} position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
+                <CuboidCollider args={[1.5,1.5,0.5]}/>
+                <CuboidCollider args={[0.25,1,0.25]} position={[0,0,1]} rotation={[-Math.PI * 0.35, 0, 0]}/>
                 <BallCollider args={[1.5]}/>
                 <mesh castShadow >
                     <torusGeometry args={[1, 0.5, 16, 32]}/>
                     <meshStandardMaterial color='mediumpurple' />
                 </mesh>
-            </RigidBody>
+            </RigidBody> */}
 
             <RigidBody type='fixed'>
                 <mesh receiveShadow position-y={- 1.25}>
