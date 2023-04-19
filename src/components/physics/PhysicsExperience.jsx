8dd92f3ae3a5f0dea9e8676e611796b25dba7fa0
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-19 20:59:56 +0100

    stress test

--- src/components/physics/PhysicsExperience.jsx
+++ src/components/physics/PhysicsExperience.jsx
@@ -1,28 +1,21 @@
 import { useGLTF, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { BallCollider, CuboidCollider, Debug, RigidBody, Physics } from '@react-three/rapier'
-import { useRef, useState } from 'react'
+import { InstancedRigidBodies, CylinderCollider, BallCollider, CuboidCollider, Debug, RigidBody, Physics } from '@react-three/rapier'
+import { useMemo, useEffect, useState, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
-export default function PhysicsExperience() {
-
+export default function Experience()
+{
     const [ hitSound ] = useState(() => new Audio('./hit.mp3'))
-
-    const cube = useRef()
     const twister = useRef()
+    const cube = useRef()
 
-    const cubeJump = () => {
-
-        const mass = cube.current.mass()
-        cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 })
-        cube.current.applyTorqueImpulse({ x: Math.random() - 0.5, y: Math.random() - 0.5, z: Math.random() - 0.5 })
-    }
-
-    useFrame((state) => {
+    useFrame((state) =>
+    {
         const time = state.clock.getElapsedTime()
-
-        const eulerRotation = new THREE.Euler( 0, time * 3, 0)
+        
+        const eulerRotation = new THREE.Euler(0, time * 3, 0)
         const quaternionRotation = new THREE.Quaternion()
         quaternionRotation.setFromEuler(eulerRotation)
         twister.current.setNextKinematicRotation(quaternionRotation)
@@ -30,15 +23,46 @@ export default function PhysicsExperience() {
         const angle = time * 0.5
         const x = Math.cos(angle) * 2
         const z = Math.sin(angle) * 2
-        twister.current.setNextKinematicTranslation({x: x, y: -0.8, z: z})
-
+        twister.current.setNextKinematicTranslation({ x: x, y: - 0.8, z: z })
     })
 
-    const collisionEnter = () => {
+    const cubeJump = () =>
+    {
+        const mass = cube.current.mass()
+        
+        cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 })
+        cube.current.applyTorqueImpulse({ x: Math.random() - 0.5, y: Math.random() - 0.5, z: Math.random() - 0.5 })
+    }
+
+    const collisionEnter = () =>
+    {
+        console.log('collision!')
         // hitSound.currentTime = 0
         // hitSound.volume = Math.random()
         // hitSound.play()
     }
+
+    const hamburger = useGLTF('./hamburger.glb')
+
+    const cubesCount = 1000
+    const cubes = useRef()
+    const cubeTransforms = useMemo(() =>
+    {
+        const positions = []
+        const rotations = []
+        const scales = []
+
+        for(let i = 0; i < cubesCount; i++)
+        {
+            positions.push([ (Math.random() - 0.5) * 8, 6 + i * 0.2, (Math.random() - 0.5) * 8 ])
+            rotations.push([ Math.random(), Math.random(), Math.random() ])
+
+            const scale = 0.2 + Math.random() * 0.8
+            scales.push([ scale, scale, scale ])
+        }
+
+        return { positions, rotations, scales }
+    }, [])
 
     return <>
 
@@ -46,67 +70,86 @@ export default function PhysicsExperience() {
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
-        <ambientLight intensity={0.5} />
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
 
-        <Physics gravity={[0, -9.81, 0]}>
-            <Debug />
+        <Physics gravity={ [ 0, - 9.08, 0 ] }>
 
-            <RigidBody colliders="ball">
-                <mesh castShadow position={[-1.5, 2, 0]}>
+            {/* <Debug /> */}
+
+            <RigidBody colliders="ball" position={ [ - 1.5, 2, 0 ] }>
+                <mesh castShadow>
                     <sphereGeometry />
                     <meshStandardMaterial color="orange" />
                 </mesh>
             </RigidBody>
 
             <RigidBody
-                ref={cube}
-                position={[1.5, 2, 0]}
-                gravityScale={1}
-                restitution={0}
-                friction={0.7}
-                colliders={false}
-                onCollisionEnter={collisionEnter}
-                // onCollisionExit={() => {console.log('exit');}}
-                // onSleep={() => console.log('sleep')}
-                // onWake={() => console.log('wake')}
+                ref={ cube }
+                position={ [ 1.5, 2, 0 ] }
+                gravityScale={ 1 }
+                restitution={ 0 }
+                friction={ 0.7 }
+                colliders={ false }
+                // onCollisionEnter={ collisionEnter }
+                // onCollisionExit={ () => { console.log('exit') } }
+                // onSleep={ () => { console.log('sleep') } }
+                // onWake={ () => { console.log('wake') } }
             >
-                <CuboidCollider mass={0.5} args={[0.5, 0.5, 0.5]} />
-                <mesh castShadow onClick={cubeJump}>
+                <mesh castShadow onClick={ cubeJump }>
                     <boxGeometry />
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
+                <CuboidCollider mass={ 2 } args={ [ 0.5, 0.5, 0.5 ] } />
             </RigidBody>
 
-            {/* <RigidBody colliders={false} position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
-                <CuboidCollider args={[1.5,1.5,0.5]}/>
-                <CuboidCollider args={[0.25,1,0.25]} position={[0,0,1]} rotation={[-Math.PI * 0.35, 0, 0]}/>
-                <BallCollider args={[1.5]}/>
-                <mesh castShadow >
-                    <torusGeometry args={[1, 0.5, 16, 32]}/>
-                    <meshStandardMaterial color='mediumpurple' />
-                </mesh>
-            </RigidBody> */}
-
-            <RigidBody type='fixed' friction={0.7}>
-                <mesh receiveShadow position-y={- 1.25}>
-                    <boxGeometry args={[10, 0.5, 10]} />
+            <RigidBody
+                type="fixed"
+                restitution={ 0 }
+                friction={ 0.7 }
+            >
+                <mesh receiveShadow position-y={ - 1.25 }>
+                    <boxGeometry args={ [ 10, 0.5, 10 ] } />
                     <meshStandardMaterial color="greenyellow" />
                 </mesh>
             </RigidBody>
 
             <RigidBody
-                position={[0, - 0.8, 0]}
-                friction={0}
+                ref={ twister }
+                position={ [ 0, - 0.8, 0 ] }
+                friction={ 0 }
                 type="kinematicPosition"
-                ref={twister}
             >
-                <mesh castShadow scale={[0.4, 0.4, 3]}>
+                <mesh castShadow scale={ [ 0.4, 0.4, 3 ] }>
                     <boxGeometry />
                     <meshStandardMaterial color="red" />
                 </mesh>
             </RigidBody>
 
+            <RigidBody colliders='trimesh' position={ [ 0, 4, 0 ] }>
+                <primitive object={ hamburger.scene } scale={ 0.25 } />
+                {/* <CylinderCollider args={ [ 0.5, 1.25 ] } /> */}
+            </RigidBody>
+
+            <RigidBody type="fixed">
+                <CuboidCollider args={ [ 5, 2, 0.5 ] } position={ [ 0, 1, 5.25 ] } />
+                <CuboidCollider args={ [ 5, 2, 0.5 ] } position={ [ 0, 1, - 5.25 ] } />
+                <CuboidCollider args={ [ 0.5, 2, 5 ] } position={ [ 5.25, 1, 0 ] } />
+                <CuboidCollider args={ [ 0.5, 2, 5 ] } position={ [ - 5.25, 1, 0 ] } />
+            </RigidBody>
+
+            <InstancedRigidBodies
+                positions={ cubeTransforms.positions }
+                rotations={ cubeTransforms.rotations }
+                scales={ cubeTransforms.scales }
+            >
+                <instancedMesh ref={ cubes } castShadow receiveShadow args={ [ null, null, cubesCount ] }>
+                    <boxGeometry />
+                    <meshStandardMaterial color="tomato" />
+                </instancedMesh>
+            </InstancedRigidBodies>
+
         </Physics>
+
     </>
 }
