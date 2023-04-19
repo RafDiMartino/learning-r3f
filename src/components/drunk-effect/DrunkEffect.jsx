a55d8708a143ba804cd555af8a0701fa4c7e65a8
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-19 12:39:29 +0100

    custom effect

---
+++ src/components/drunk-effect/DrunkEffect.jsx
@@ -0,0 +1,41 @@
+import { Effect, BlendFunction } from "postprocessing";
+import { Uniform } from "three";
+
+const fragmentShader = /* glsl */`
+
+    uniform float frequency;
+    uniform float amplitude;
+    uniform float offset;
+
+    void mainUv(inout vec2 uv)
+    {
+        uv.y += sin(uv.x * frequency + offset) * amplitude;
+    }
+
+    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
+    {
+        outputColor = vec4(0.8, 1.0, 0.5, inputColor.a);
+    }
+`
+
+export default class DrunkEffect extends Effect{
+    constructor({frequency, amplitude, blendFunction = BlendFunction.SCREEN}){
+        super(
+            'DrunkEffect', 
+            fragmentShader, 
+            {
+                blendFunction: blendFunction,
+                uniforms: new Map([
+                    ['frequency', new Uniform(frequency)],
+                    ['amplitude', new Uniform(amplitude)],
+                    ['offset', new Uniform(0)]
+                ])
+            }
+        )
+    }
+
+    update(renderer, inputBuffer, deltaTime){
+        this.uniforms.get('offset').value += deltaTime
+    }
+
+}
