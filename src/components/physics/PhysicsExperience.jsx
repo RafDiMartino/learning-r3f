cbf1a380790e714257b5d20fa56116f31853bc1c
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-19 18:38:14 +0100

    object settings

--- src/components/physics/PhysicsExperience.jsx
+++ src/components/physics/PhysicsExperience.jsx
@@ -2,16 +2,35 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { BallCollider, CuboidCollider, Debug, RigidBody, Physics } from '@react-three/rapier'
 import { useRef } from 'react'
-
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 
 export default function PhysicsExperience() {
 
     const cube = useRef()
+    const twister = useRef()
 
     const cubeJump = () => {
-        cube.current.applyImpulse({x: 0, y: 5, z: 0})
-        cube.current.applyTorqueImpulse({x: Math.random() -0.5, y: Math.random() -0.5, z: Math.random() -0.5})
+
+        const mass = cube.current.mass()
+        cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 })
+        cube.current.applyTorqueImpulse({ x: Math.random() - 0.5, y: Math.random() - 0.5, z: Math.random() - 0.5 })
     }
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
+
+        const eulerRotation = new THREE.Euler( 0, time * 3, 0)
+        const quaternionRotation = new THREE.Quaternion()
+        quaternionRotation.setFromEuler(eulerRotation)
+        twister.current.setNextKinematicRotation(quaternionRotation)
+
+        const angle = time * 0.5
+        const x = Math.cos(angle) * 2
+        const z = Math.sin(angle) * 2
+        twister.current.setNextKinematicTranslation({x: x, y: -0.8, z: z})
+
+    })
 
     return <>
 
@@ -22,7 +41,7 @@ export default function PhysicsExperience() {
         <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-        <Physics>
+        <Physics gravity={[0, -9.81, 0]}>
             <Debug />
 
             <RigidBody colliders="ball">
@@ -32,7 +51,15 @@ export default function PhysicsExperience() {
                 </mesh>
             </RigidBody>
 
-            <RigidBody ref={cube} position={[1.5, 2, 0]}>
+            <RigidBody
+                ref={cube}
+                position={[1.5, 2, 0]}
+                gravityScale={1}
+                restitution={0}
+                friction={0.7}
+                colliders={false}
+            >
+                <CuboidCollider mass={0.5} args={[0.5, 0.5, 0.5]} />
                 <mesh castShadow onClick={cubeJump}>
                     <boxGeometry />
                     <meshStandardMaterial color="mediumpurple" />
@@ -49,10 +76,22 @@ export default function PhysicsExperience() {
                 </mesh>
             </RigidBody> */}
 
-            <RigidBody type='fixed'>
+            <RigidBody type='fixed' friction={0.7}>
                 <mesh receiveShadow position-y={- 1.25}>
                     <boxGeometry args={[10, 0.5, 10]} />
                     <meshStandardMaterial color="greenyellow" />
+                </mesh>
+            </RigidBody>
+
+            <RigidBody
+                position={[0, - 0.8, 0]}
+                friction={0}
+                type="kinematicPosition"
+                ref={twister}
+            >
+                <mesh castShadow scale={[0.4, 0.4, 3]}>
+                    <boxGeometry />
+                    <meshStandardMaterial color="red" />
                 </mesh>
             </RigidBody>
 
