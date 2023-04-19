66edf972fde2a16183971ad46afb3d54129580d6
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-19 17:01:39 +0100

    colliders

--- src/components/physics/PhysicsExperience.jsx
+++ src/components/physics/PhysicsExperience.jsx
@@ -1,31 +1,55 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { BallCollider, CuboidCollider, Debug, RigidBody, Physics } from '@react-three/rapier'
 
-export default function PhysicsExperience()
-{
+export default function PhysicsExperience() {
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
 
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <Physics>
+            <Debug />
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <RigidBody colliders="ball">
+                <mesh castShadow position={[0, 4, 0]}>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="orange" />
+                </mesh>
+            </RigidBody>
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            {/* <RigidBody >
+                <mesh castShadow position={[2, 2, 0]}>
+                    <boxGeometry args={[3, 2, 1]} />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+                <mesh castShadow position={[2, 2, 3]}>
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody> */}
 
+            <RigidBody colliders={false} position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
+                {/* <CuboidCollider args={[1.5,1.5,0.5]}/>
+                <CuboidCollider args={[0.25,1,0.25]} position={[0,0,1]} rotation={[-Math.PI * 0.35, 0, 0]}/> */}
+                <BallCollider args={[1.5]}/>
+                <mesh castShadow >
+                    <torusGeometry args={[1, 0.5, 16, 32]}/>
+                    <meshStandardMaterial color='mediumpurple' />
+                </mesh>
+            </RigidBody>
+
+            <RigidBody type='fixed'>
+                <mesh receiveShadow position-y={- 1.25}>
+                    <boxGeometry args={[10, 0.5, 10]} />
+                    <meshStandardMaterial color="greenyellow" />
+                </mesh>
+            </RigidBody>
+
+        </Physics>
     </>
 }
