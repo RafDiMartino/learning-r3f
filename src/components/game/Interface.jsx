4a5a3f9ea49006e641252999dbfc89bb7b90d1e1
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-21 17:09:06 +0100

    adding game mechanics

--- src/components/game/Interface.jsx
+++ src/components/game/Interface.jsx
@@ -1,6 +1,15 @@
 import { useKeyboardControls } from "@react-three/drei"
+import { useRef, useEffect } from "react"
+import { addEffect } from "@react-three/fiber"
+import useGame from "./stores/useGame"
+
 
 export default function Interface() {
+
+    const time = useRef()
+
+    const restart = useGame((state) => state.restart)
+    const phase = useGame((state) => state.phase)
 
     const forward = useKeyboardControls((state) => state.forward)
     const backward = useKeyboardControls((state) => state.backward)
@@ -8,12 +17,39 @@ export default function Interface() {
     const rightward = useKeyboardControls((state) => state.rightward)
     const jump = useKeyboardControls((state) => state.jump)
 
+    useEffect(() => {
+        const unsubscribeEffect = addEffect(() => {
+
+            const state = useGame.getState()
+
+            let elapsedTime = 0
+
+            if (state.phase === 'playing')
+                elapsedTime = Date.now() - state.startTime
+            else if (state.phase === 'ended')
+                elapsedTime = state.endTime - state.startTime
+
+            elapsedTime /= 1000
+            elapsedTime = elapsedTime.toFixed(2)
+
+            if(time.current) {
+                time.current.textContent = elapsedTime
+            }
+
+        })
+
+        return () => {
+            unsubscribeEffect()
+        }
+
+    }, [])
+
     return (
         <div className="interface">
 
-            <div className="time">0.00</div>
+            <div className="time" ref={time}>0.00</div>
 
-            <div className="restart">Restart</div>
+            {phase === 'ended' && <div className="restart" onClick={restart}>Restart</div>}
 
             <div className="controls">
                 <div className="raw">
